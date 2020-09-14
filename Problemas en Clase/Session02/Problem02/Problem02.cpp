13ccdcdf0d871a8ccf89722ef27b7ef049695e15
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-09-14 13:00:42 -0500

    Added resolution of the problem

--- Problemas en Clase/Session02/Problem02/Problem02.cpp
+++ Problemas en Clase/Session02/Problem02/Problem02.cpp
@@ -1,20 +1,36 @@
-// Problem02.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <math.h>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Declaración de variables y constantes
+    double valueX; // double para mayor rango
+    double valueA; // double para mayor rango
+    double valueB; // double para mayor rango
+    double valueC; // double para mayor rango
+    const unsigned long valueG = 1.5;
+
+    // Entrada de datos
+
+    cout << "BIENVENIDO AL PROBLEMA #02" << endl;
+    cout << "Resuelto por el grupo #03" << endl;
+
+    cout << "\tPor favor ingrese el valor de la variable 'A':";
+    cin >> valueA;
+
+    cout << "\tPor favor ingrese el valor de la variable 'B':";
+    cin >> valueB;
+
+    cout << "\tPor favor ingrese el valor de la variable 'C':";
+    cin >> valueC;
+
+    // Proceso
+    valueX = ((pow(valueB, 2) - (valueC * 3)) / (valueA * valueB + 1)) + valueG - (valueA / valueB);
+
+    // Salida de datos
+    cout << "El valor final es: " << endl << "x=" << valueX << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
