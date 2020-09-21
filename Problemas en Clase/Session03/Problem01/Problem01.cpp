9839b4bb90d30ce31034d652d7d87de9ff18c876
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-09-21 09:13:28 -0500

    Added content to Problem01

--- Problemas en Clase/Session03/Problem01/Problem01.cpp
+++ Problemas en Clase/Session03/Problem01/Problem01.cpp
@@ -1,20 +1,43 @@
-// Problem01.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Declaración de variables y constantes
+    char firstName[100];
+    unsigned short age;
+    float price;
+
+    // Entrada de datos
+
+    cout << "\tPor favor ingrese el nombre de la persona (max. 100 caracteres) : ";
+    fflush(stdin);
+    cin.getline(firstName, 100);
+
+    cout << "\tPor favor ingrese la edad de la persona (años):";
+    cin >> age;
+
+    // Proceso
+    system("cls");
+
+    if (age < 5) {
+        price = 5;
+    }
+    else if (age >= 5 && age < 10) {
+        price = 10;
+    }
+    else if (age >= 10 && age < 18) {
+        price = 15;
+    }
+    else if (age >= 18) {
+        price = 20;
+    }
+
+    // Salida de datos
+    cout << "El cliente " << firstName << " pagará para la entrada es de S/ " << price << "." << endl;
+
+    system("pause");
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
