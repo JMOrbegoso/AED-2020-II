b868828e976a630da7be76f4ab65f9ef2c0a4964
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-09-21 19:33:50 -0500

    Update Problem16.cpp
    Added content.

--- Laboratorios/PA1/Problem16/Problem16.cpp
+++ Laboratorios/PA1/Problem16/Problem16.cpp
@@ -1,20 +1,90 @@
-// Problem16.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <windows.h>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Declaración de variables y constantes
+    char firstName[100];
+    char lastName[100];
+    unsigned short chosenNumber;
+    float originalAmount, netAmount;
+    float discount = 0;
+
+    // Entrada de datos
+
+    cout << "BIENVENIDO AL PROBLEMA #16" << endl;
+    cout << "Resuelto por el grupo #03" << endl << endl << endl;
+
+    cout << "Por favor ingrese el nombre del cliente (max. 100 caracteres): ";
+    fflush(stdin);
+    cin.getline(firstName, 100);
+
+    cout << "Por favor ingrese el apellido del cliente (max. 100 caracteres): ";
+    fflush(stdin);
+    cin.getline(lastName, 100);
+
+    cout << "Por favor ingrese el monto original de la compra (S/):";
+    cin >> originalAmount;
+
+    cout << "Por favor ingrese el número elegido por el cliente:";
+    cin >> chosenNumber;
+
+    // Proceso
+
+    // Se limpia la pantalla
+    system("cls");
+
+    switch (chosenNumber)
+    {
+    case 1:
+    case 3:
+        if (originalAmount > 100) {
+            discount = 15;
+        }
+        break;
+
+    case 2:
+    case 9:
+        discount = 10 * originalAmount / 100;
+        break;
+
+    case 4:
+    case 7:
+        discount = 15 * originalAmount / 100;
+        break;
+
+    case 5:
+        if (originalAmount < 50) {
+            discount = originalAmount;
+        }
+        break;
+
+    default:
+        discount = 0;
+        break;
+    }
+
+    netAmount = originalAmount - discount;
+
+    // Datos de salida
+
+    cout << "El cliente " << firstName << " " << lastName;
+    if (discount == 0) {
+        cout << " no ha obtenido ningun descuento";
+    }
+    else {
+        cout << " ha obtenido descuento de S/" << discount;
+    }
+
+    Sleep(1000);
+
+    cout << ", y deberá pagar S/" << netAmount << " por su compra.";
+
+    cout << endl;
+
+    system("pause");
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
