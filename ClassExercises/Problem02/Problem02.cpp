b389a6b072cbcf783677529642c4246dab7051c4
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-09-28 08:55:50 -0500

    Update Problem02.cpp
    Added content.

--- ClassExercises/Problem02/Problem02.cpp
+++ ClassExercises/Problem02/Problem02.cpp
@@ -1,20 +1,94 @@
-// Problem02.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <windows.h>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Declaración de variables y constantes
+    unsigned short breadOption; // 0: Pan Frances, 1: Panetón, 2: Croissant, 3: Ciabata
+    unsigned short quantity;
+    char fullName[100];
+    char ruc[11];
+    float breadPrice, igvAmount, discount, grossAmount, netAmount;
+    const short igv = 18;
+    const float frenchBreadPrice = 0.30;
+    const float panettonePrice = 15.50;
+    const float croissantPrice = 0.40;
+    const float ciabattaPrice = 0.60;
+
+    // Entrada de datos
+
+    cout << "BIENVENIDO A LA PANADERIA" << endl;
+
+    cout << "\tPor favor ingrese su nombre y apellidos completos (max. 100 letras):";
+    fflush(stdin);
+    cin.getline(fullName, 100);
+
+    cout << "\tPor favor ingrese su RUC (max. 11 caracteres):";
+    fflush(stdin);
+    cin.getline(ruc, 11);
+
+    cout << "\tPor favor ingrese el número del pan deseado [0: Pan Francés, 1: Panetón, 2: Croissant, 3: Ciabata]:";
+    cin >> breadOption;
+
+    cout << "\tPor favor ingrese la cantidad de panes deseado:";
+    cin >> quantity;
+
+    // Proceso
+    system("cls");
+
+    switch (breadOption) {
+    case 0:
+        breadPrice = frenchBreadPrice;
+        break;
+    case 1:
+        breadPrice = panettonePrice;
+        break;
+    case 2:
+        breadPrice = croissantPrice;
+        break;
+    case 3:
+        breadPrice = ciabattaPrice;
+        break;
+    default:
+        cout << "\tEl tipo de pan no es válido, intente otra vez.";
+        return 0;
+        break;
+    }
+
+    grossAmount = breadPrice * quantity;
+
+    igvAmount = igv * grossAmount / 100;
+    grossAmount = igvAmount + grossAmount;
+
+    if (20 <= grossAmount) {
+        discount = 0;
+    }
+    else if (20 < grossAmount && 35 < grossAmount) {
+        discount = 2 * grossAmount / 100;
+    }
+    else if (35 < grossAmount && 50 < grossAmount) {
+        discount = 5 * grossAmount / 100;
+    }
+    else {
+        discount = 10 * grossAmount / 100;
+    }
+
+    netAmount = grossAmount - discount;
+
+    // Salida de datos
+    cout << "El cliente " << fullName << " con RUC " << ruc << endl;
+    Sleep(1000);
+    cout << "Ha comprado " << quantity << " panes del tipo " << breadOption << endl;
+    Sleep(1000);
+    cout << "Por lo que debe de pagar S/" << netAmount << "." << endl;
+    Sleep(1000);
+    cout << "Su IGV fue de S/" << igvAmount << "." << endl;
+    Sleep(1000);
+    cout << "Su descuento fue de S/" << discount << "." << endl;
+
+    system("pause");
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
