ebfa3b706bd379f7d1d17e0a92696a2e3206308e
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-10-03 08:42:56 -0500

    Update Problem03.cpp
    Added content writted by Edward.

--- Laboratorios/PA1/Problem03/Problem03.cpp
+++ Laboratorios/PA1/Problem03/Problem03.cpp
@@ -1,20 +1,40 @@
-// Problem03.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float montobon, montototal, sueldo;
+    unsigned short year;
+
+    cout << "Bienvenido a la Empresa S.A. SKIPTER" << endl;
+
+    cout << "Ingrese el sueldo base S/: " << endl;
+    cin >> sueldo;
+
+    cout << "Ingrese la cantidad de años de servicio a la empresa: " << endl;
+    cin >> year;
+
+    if (year > 10) {
+        montobon = 0.45 * sueldo;
+    }
+    else if (year >= 7 && year <= 10) {
+        montobon = 0.35 * sueldo;
+    }
+    else if (year >= 4 && year <= 8) {
+        montobon = 0.25 * sueldo;
+    }
+    else if (year >= 1 && year <= 3) {
+        montobon = 0.15 * sueldo;
+    }
+    else {
+        montobon = 0;
+    }
+
+    montototal = sueldo + montobon;
+
+    cout << "El monto de bonificación es S/" << montobon << endl;
+    cout << "El monto total a pagar al empleado es S/" << montototal << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
