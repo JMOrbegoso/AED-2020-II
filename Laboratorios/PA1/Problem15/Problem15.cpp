9f9b2103b1141e87be2f6dfc150a5d3c9515586e
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-10-03 08:49:24 -0500

    Update Problem15.cpp
    Added content writted by Edward.

--- Laboratorios/PA1/Problem15/Problem15.cpp
+++ Laboratorios/PA1/Problem15/Problem15.cpp
@@ -1,20 +1,84 @@
-// Problem15.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    short puntaje1 = 0, puntaje2 = 0, puntaje3 = 0, puntaje4 = 0, puntaje5 = 0, puntajetotal;
+    char alternativa1, alternativa2, alternativa3, alternativa4, alternativa5;
+
+    cout << "BIENVENIDO AL PROGRAMA INTELECTO PERUANO";
+    cout << "Para las preguntas planteadas debes responder con una sola alternativa SI(S)o NO(N)";
+    cout << "Empecemos con tu intelecto peruano";
+
+    cout << "/t/La moneda nacional del PERU es el dolar (SI o NO).";
+    cin >> alternativa1;
+
+    cout << "/t/En Arequipa nace el rio Amazonas(SI o NO).";
+    cin >> alternativa2;
+
+    cout << "/t/El territorio peruano constituye de 25 departamentos(SI o NO).";
+    cin >> alternativa3;
+
+    cout << "/t/El 28 de julio se celebra la Batalla de Angamos(SI o NO).";
+    cin >> alternativa4;
+
+    cout << "/t/En el escudo nacional se encuentra el arbol de la quina(SI o NO)";
+    cin >> alternativa5;
+
+    if (alternativa1 == 'S') {
+        puntaje1 = -1;
+    }
+    else if (alternativa1 == 'N') {
+        puntaje1 = +2;
+    }
+    else {
+        cout << "CODIGO INGRESADO ERRONEO";
+    }
+
+    if (alternativa2 == 'S') {
+        puntaje2 = +2;
+    }
+    else if (alternativa2 == 'N') {
+        puntaje2 = -1;
+    }
+    else {
+        cout << "CODIGO INGRESADO ERRONEO";
+    }
+
+    if (alternativa3 == 'S') {
+        puntaje3 = -1;
+    }
+    else if (alternativa3 == 'N') {
+        puntaje3 = +2;
+    }
+    else {
+        cout << "CODIGO INGRESADO ERRONEO";
+    }
+
+    if (alternativa4 == 'S') {
+        puntaje4 = -1;
+    }
+    else if (alternativa4 == 'N') {
+        puntaje4 = +2;
+    }
+    else {
+        cout << "CODIGO INGRESADO ERRONEO";
+    }
+
+    if (alternativa5 == 'S') {
+        puntaje5 = +2;
+    }
+    else if (alternativa5 == 'N') {
+        puntaje5 = -1;
+    }
+    else {
+        cout << "CODIGO INGRESADO ERRONEO";
+    }
+
+    puntajetotal = (puntaje1 + puntaje2 + puntaje3 + puntaje4 + puntaje5);
+
+    cout << "El puntaje total del jugador es: " << puntajetotal << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
