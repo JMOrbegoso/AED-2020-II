259148fbe2da3b22c11e5da43a8d2d3384922959
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-10-19 08:46:57 -0500

    Update Problem02.cpp

--- ClassExercises/Week07/Problem02/Problem02.cpp
+++ ClassExercises/Week07/Problem02/Problem02.cpp
@@ -3,31 +3,31 @@
 //entre ellos.
 
 #include <iostream>
-#include <windows.h>
 
 using namespace std;
 
 // Forward declarations
 int getSeletectOption();
-void startProcess();
+void requestNumbers(short& firstNumber, short& secondNumber);
+void startProcess(short firstNumber, short secondNumber);
 void showProblem();
 void showCredits();
 
 int main()
 {
     unsigned short seletectMenuOption;
+    short firstNumber;
+    short secondNumber;
 
     do {
         seletectMenuOption = getSeletectOption();
-
-        cout << "selecionó la opción" << seletectMenuOption << endl;
-        Sleep(2000);
 
         if (seletectMenuOption != 0) {
             switch (seletectMenuOption)
             {
             case 1:
-                void startProcess();
+                requestNumbers(firstNumber, secondNumber);
+                startProcess(firstNumber, secondNumber);
                 break;
 
             case 2:
@@ -56,7 +56,7 @@ int getSeletectOption() {
     cout << "\tBienvenido al Problema 02" << endl;
     cout << "-----------------------------------------" << endl;
     cout << "Desarrollado por Juan Manuel Orbegoso" << endl;
-    cout << "https://www.jmorbegoso.com/" << endl;
+    cout << "https://www.jmorbegoso.com" << endl;
     cout << "-----------------------------------------" << endl;
 
     cout << "[1] Empezar" << endl;
@@ -74,12 +74,36 @@ int getSeletectOption() {
     return 0;
 }
 
-void startProcess() {
+void requestNumbers(short& firstNumber, short& secondNumber) {
 
+    cout << "Introduzca el primer número entero (menor que 1000):" << endl;
+    cin >> firstNumber;
+    while (!(firstNumber < 1000)) {
+        cout << "Por favor introduzca un número menor que 1000:";
+        cin >> firstNumber;
+    }
+
+    cout << "Introduzca el segundo número entero (menor que 1000):" << endl;
+    cin >> secondNumber;
+    while (!(secondNumber < 1000)) {
+        cout << "Por favor introduzca un número menor que 1000:";
+        cin >> secondNumber;
+    }
+}
+
+void startProcess(short firstNumber, short secondNumber) {
+    short sum = firstNumber + secondNumber;
+    short average = sum / 2;
+
+    cout << "La suma de los números ingresados es: " << sum << endl;
+    cout << "Y su promedio es de : " << average << endl;
+    cout << "-----Fin del Problema------" << endl;
 }
 
 void showProblem() {
-    cout << "Elabore un programa que lea 2 números enteros.Validar que no sea iguales no mayores que 1000. Luego de ello debe hallar la suma y el promedio de los números comprendidos entre ellos." << endl;
+    cout << "Elabore un programa que lea 2 números enteros." << endl;
+    cout << "Validar que no sea iguales no mayores que 1000. " << endl;
+    cout << "Luego de ello debe hallar la suma y el promedio de los números comprendidos entre ellos." << endl;
 }
 
 void showCredits() {
