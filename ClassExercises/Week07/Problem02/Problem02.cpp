1b4d93953cc43b84f97242f1652a510822218158
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-10-19 08:36:47 -0500

    Update Problem02.cpp

--- ClassExercises/Week07/Problem02/Problem02.cpp
+++ ClassExercises/Week07/Problem02/Problem02.cpp
@@ -1,20 +1,88 @@
-// Problem02.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Elabore un programa que lea 2 números enteros.Validar que no sea iguales no mayores
+//que 1000. Luego de ello debe hallar la suma y el promedio de los números comprendidos
+//entre ellos.
 
 #include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+// Forward declarations
+int getSeletectOption();
+void startProcess();
+void showProblem();
+void showCredits();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    unsigned short seletectMenuOption;
+
+    do {
+        seletectMenuOption = getSeletectOption();
+
+        cout << "selecionó la opción" << seletectMenuOption << endl;
+        Sleep(2000);
+
+        if (seletectMenuOption != 0) {
+            switch (seletectMenuOption)
+            {
+            case 1:
+                void startProcess();
+                break;
+
+            case 2:
+                showProblem();
+                break;
+
+            case 3:
+                showCredits();
+                break;
+
+            default:
+                break;
+            }
+        }
+
+    } while (!(seletectMenuOption == 0));
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int getSeletectOption() {
+    int seletectOption;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    system("cls");
+
+    cout << "\tBienvenido al Problema 02" << endl;
+    cout << "-----------------------------------------" << endl;
+    cout << "Desarrollado por Juan Manuel Orbegoso" << endl;
+    cout << "https://www.jmorbegoso.com/" << endl;
+    cout << "-----------------------------------------" << endl;
+
+    cout << "[1] Empezar" << endl;
+    cout << "[2] Ver planteamiento del problema" << endl;
+    cout << "[3] Ver Creditos" << endl;
+    cout << "[0] Salir" << endl;
+
+    cout << "Introduzca la opción deseada:" << endl;
+    cin >> seletectOption;
+    while (!(0 <= seletectOption && seletectOption <= 3)) {
+        cout << "Introdujo una opción incorrecta, por favor seleccione una opción válida:";
+        cin >> seletectOption;
+    }
+
+    return 0;
+}
+
+void startProcess() {
+
+}
+
+void showProblem() {
+    cout << "Elabore un programa que lea 2 números enteros.Validar que no sea iguales no mayores que 1000. Luego de ello debe hallar la suma y el promedio de los números comprendidos entre ellos." << endl;
+}
+
+void showCredits() {
+    cout << "Desarrollado por Juan Manuel Orbegoso" << endl;
+    cout << "https://www.jmorbegoso.com/" << endl;
+}
