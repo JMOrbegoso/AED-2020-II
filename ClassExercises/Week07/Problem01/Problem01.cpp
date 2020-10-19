65f37d1187959c8462c74d87cddb658235de127f
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-10-19 08:22:54 -0500

    Update Problem01.cpp

--- ClassExercises/Week07/Problem01/Problem01.cpp
+++ ClassExercises/Week07/Problem01/Problem01.cpp
@@ -1,20 +1,68 @@
-// Problem01.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+// Forward declarations
+int getSeletectOption();
+void leerDatos(char nombre[40], char dni[8], float& sueldo);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    unsigned short seletectMenuOption;
+
+    do {
+
+        seletectMenuOption = getSeletectOption();
+
+        cout << "selecionó la opción" << seletectMenuOption;
+
+        Sleep(2000);
+
+    } while (!(seletectMenuOption == 0));
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int getSeletectOption() {
+    int seletectOption;
+    char nombre[40];
+    char dni[9];
+    float sueldo;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    system("cls");
+    cout << "1. Entrar" << endl;
+    cout << "0. Salir" << endl;
+
+    // do {
+    //     cout << "Introduzca la opción deseada:";
+    //    cin >> seletectOption;
+    //  } while (!(0 <= seletectOption && seletectOption <= 5));
+
+    leerDatos(nombre, dni, sueldo);
+
+    cout << "Introduzca la opción deseada:";
+    cin >> seletectOption;
+    while (!(0 <= seletectOption && seletectOption <= 5)) {
+        cout << "Introdujo un valor incorrecto, por favor seleccione una opción válida:";
+        cin >> seletectOption;
+    }
+
+    return 0;
+}
+
+void leerDatos(char nombre[40], char dni[9], float& sueldo) {
+
+    system("cls");
+
+    cout << "Ingrese nombre:";
+    fflush(stdin);
+    cin.getline(nombre, 40);
+
+    cout << "Ingrese DNI:";
+    fflush(stdin);
+    cin.getline(dni, 9);
+
+    cout << "Ingrese sueldo:";
+    cin >> sueldo;
+}
