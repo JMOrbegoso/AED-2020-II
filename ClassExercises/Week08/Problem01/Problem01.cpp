242e5606acf92ffe9a92f6ab409f80a31989618d
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-10-26 09:41:45 -0500

    Update Problem01.cpp

--- ClassExercises/Week08/Problem01/Problem01.cpp
+++ ClassExercises/Week08/Problem01/Problem01.cpp
@@ -1,20 +1,161 @@
-// Problem01.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Leer las edades de un conjunto de estudiantes y:
+// -> Calcular el promedio de edades
+// -> Calcular la edad del mayor
+// -> Calcucar la edad del menor
+// -> Mostrar cuantos son menores y mayores de edad
 
 #include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+// Forward declarations
+int getSeletectOption();
+void requestStudentAge(int* studentsAges, int& studentsQuantity);
+void showResults(int* studentsAges, int studentsQuantity);
+void showProblem();
+void showCredits();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    unsigned short seletectMenuOption;
+    int studentsAges[1000];
+    int studentsQuantity = 0;
+
+    do {
+        seletectMenuOption = getSeletectOption();
+
+        if (seletectMenuOption != 0) {
+            switch (seletectMenuOption)
+            {
+            case 1:
+                requestStudentAge(studentsAges, studentsQuantity);
+                break;
+
+            case 2:
+                showResults(studentsAges, studentsQuantity);
+                break;
+
+            case 3:
+                showProblem();
+                break;
+
+            case 4:
+                showCredits();
+                break;
+
+            default:
+                break;
+            }
+
+            Sleep(1000);
+        }
+
+    } while (!(seletectMenuOption == 0));
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int getSeletectOption() {
+    int seletectOption;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    system("cls");
+
+    cout << "\tBienvenido al Problema 02" << endl;
+    cout << "-----------------------------------------" << endl;
+    cout << "Desarrollado por Juan Manuel Orbegoso" << endl;
+    cout << "https://www.jmorbegoso.com" << endl;
+    cout << "-----------------------------------------" << endl;
+
+    cout << "[1] Ingresar edad de estudiante" << endl;
+    cout << "[2] Ver resultados" << endl;
+    cout << "[3] Ver planteamiento del problema" << endl;
+    cout << "[4] Ver Creditos" << endl;
+    cout << "[0] Salir" << endl;
+
+    cout << "Introduzca la opción deseada:" << endl;
+    cin >> seletectOption;
+    while (!(0 <= seletectOption && seletectOption <= 3)) {
+        cout << "Introdujo una opción incorrecta, por favor seleccione una opción válida:";
+        cin >> seletectOption;
+    }
+
+    return seletectOption;
+}
+
+void requestStudentAge(int* studentsAges, int& studentsQuantity) {
+    int studentAge;
+
+    cout << "Introduzca la edad del estudiante [de 5 años a 100 años]:" << endl;
+    cin >> studentAge;
+
+    while (!(studentAge > 4)) {
+        cout << "Por favor introduzca una edad mayor que 3:";
+        cin >> studentAge;
+    }
+
+    while (!(studentAge < 101)) {
+        cout << "Por favor introduzca una edad menor que 100:";
+        cin >> studentAge;
+    }
+
+    studentsAges[studentsQuantity] = studentAge;
+
+    studentsQuantity++;
+}
+
+void showResults(int* studentsAges, int studentsQuantity) {
+    int agesSum = 0;
+    int maxAge = 0;
+    int minAge = 100;
+    int adultsQuantity = 0;
+    int youngsQuantity = 0;
+    float averageAge;
+
+    for (int i = 0; i < studentsQuantity; i++) {
+        agesSum += studentsAges[i];
+
+        if (studentsAges[i] > maxAge) {
+            maxAge = studentsAges[i];
+        }
+
+        if (studentsAges[i] < minAge) {
+            minAge = studentsAges[i];
+        }
+
+        if (18 <= studentsAges[i]) {
+            adultsQuantity++;
+        }
+        else {
+            youngsQuantity++;
+        }
+    }
+
+    averageAge = (float)agesSum / (float)studentsQuantity;
+
+    cout << "Tiene " << studentsQuantity << " estudiantes." << endl;
+    cout << "El promedio de edades es de: " << averageAge << endl;
+    cout << "La edad del mayor es " << maxAge << endl;
+    cout << "La edad del menor es " << minAge << endl;
+    cout << "Hay " << adultsQuantity << " mayores de edad" << endl;
+    cout << "Hay " << youngsQuantity << " menores de edad" << endl;
+
+    system("pause");
+}
+
+void showProblem() {
+    cout << "Leer las edades de un conjunto de estudiantes y" << endl;
+    cout << "\t-> Calcular el promedio de edades." << endl;
+    cout << "\t-> Calcular la edad del mayor." << endl;
+    cout << "\t-> Calcucar la edad del menor." << endl;
+    cout << "\t-> Mostrar cuantos son menores y mayores de edad." << endl;
+
+    Sleep(3000);
+}
+
+void showCredits() {
+    cout << "Desarrollado por Juan Manuel Orbegoso" << endl;
+    cout << "https://www.jmorbegoso.com" << endl;
+
+    Sleep(3000);
+}
