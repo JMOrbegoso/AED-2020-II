2b613310ba2484818ff718fbc158686e2f588106
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-11-19 21:20:30 -0500

    Added Structs.h

---
+++ PA2/Problem08/Structs.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+struct patient {
+	string firstName;
+	string lastName;
+	char genre;
+	char dni[8];
+	int age;
+	float payment;
+	int medicalSpeciality;
+};
