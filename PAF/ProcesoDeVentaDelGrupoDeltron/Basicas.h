8700d6a988c02d978c369b4d6b4dff24381eed69
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-11-28 13:22:43 -0500

    Updated library dependencies

--- PAF/ProcesoDeVentaDelGrupoDeltron/Basicas.h
+++ PAF/ProcesoDeVentaDelGrupoDeltron/Basicas.h
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include "BasicasDeEstructuras.h"
 
 using namespace std;
 
--- PAF/ProcesoDeVentaDelGrupoDeltron/ProcesoDeVentaDelGrupoDeltron.cpp
+++ PAF/ProcesoDeVentaDelGrupoDeltron/ProcesoDeVentaDelGrupoDeltron.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "Basicas.h"
-#include "BasicasDeEstructuras.h"
 
 using namespace std;
 
