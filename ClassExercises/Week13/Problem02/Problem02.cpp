1d100846834a6a16cd905ce45f33cf50c4b0ea81
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-11-30 09:05:35 -0500

    Update Problem02.cpp

--- ClassExercises/Week13/Problem02/Problem02.cpp
+++ ClassExercises/Week13/Problem02/Problem02.cpp
@@ -1,20 +1,29 @@
-// Problem02.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* Deseamos guardar los datos de un conjunto de productos de una tienda
+Producto:
+    -Codigo
+    -Nombre
+    -Stock
+    -Marca
+
+Crear una lista enlazada simple para los productos.
+*/
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+struct product {
+    string code;
+    string name;
+    string brand;
+    int stock;
+};
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+struct node {
+    product product;
+};
