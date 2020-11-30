1cd4308b5fb3a97c600e57152fbbfb7599ce7ddc
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-11-30 08:44:50 -0500

    Update Problem01.cpp

--- ClassExercises/Week13/Problem01/Problem01.cpp
+++ ClassExercises/Week13/Problem01/Problem01.cpp
@@ -1,20 +1,25 @@
-// Problem01.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x;
+    int y;
+    float monto;
+    string nombre;
+
+    int* pX;
+    int* pY;
+    float* pMonto;
+    string* pNombre;
+
+    // puntero pX que apunta a X
+    pX = &x;
+
+    // pY que apunta a lo que apunta pX
+    pX = pY;
+
+    // Incremente el valor de x en 10 a través del puntero
+    *pX = *pX + 10;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
