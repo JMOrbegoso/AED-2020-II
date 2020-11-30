231569adeadbe6934271cf240b28a728659d9e2d
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-11-30 09:29:37 -0500

    Update Problem03.cpp

--- ClassExercises/Week13/Problem03/Problem03.cpp
+++ ClassExercises/Week13/Problem03/Problem03.cpp
@@ -1,20 +1,126 @@
-// Problem03.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* Deseamos guardar los datos de un conjunto de productos de una tienda
+Estudiante:
+    -Codigo
+    -Nombre
+    -Edad
+    -5 notas
+    -Promedio
+    -Carrera profesional
+
+Crear una lista enlazada simple.
+*/
 
 #include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+
+
+struct student {
+    string code;
+    string name;
+    int age;
+    float scores[5];
+    float averageScore;
+    string professionalCareer;
+};
+
+struct node {
+    student student;
+    node* next;
+};
+
+struct studentsList {
+    node *first;
+    int size;
+};
+
+void initList(studentsList &classroom) {
+    classroom.first = NULL;
+    classroom.size = 0;
+}
+
+void registerStudent(studentsList& classroom) {
+
+    classroom.;
+
+    classroom.size++;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    studentsList classroom;
+
+    initList(classroom);
+
+    int seletectMenuOption;
+
+    do {
+        seletectMenuOption = getSeletectOption();
+
+        if (seletectMenuOption != 0) {
+            switch (seletectMenuOption)
+            {
+            case 1:
+                registerStudent(classroom);
+                Sleep(750);
+                break;
+
+            case 2:
+                
+                system("pause");
+                break;
+
+            case 3:
+                
+                system("pause");
+                break;
+
+            case 4:
+                
+                system("pause");
+                break;
+
+            case 5:
+
+                system("pause");
+                break;
+
+            default:
+                break;
+            }
+        }
+
+    } while (!(seletectMenuOption == 0));
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int getSeletectOption() {
+    int seletectOption;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    system("cls");
+
+    cout << "\tBienvenido al Problema 03" << endl;
+    cout << "-----------------------------------------" << endl;
+    cout << "Desarrollado por Juan Manuel Orbegoso" << endl;
+    cout << "https://www.jmorbegoso.com" << endl;
+    cout << "-----------------------------------------" << endl;
+
+    cout << "[1] Registrar estudiante" << endl;
+    cout << "[2] Lista de estudiantes" << endl;
+    cout << "[3] Buscar estudiante" << endl;
+    cout << "[4] Eliminar estudiante" << endl;
+    cout << "[5] Ordernar estudiantes" << endl;
+    cout << "[0] Salir" << endl;
+
+    cout << "Introduzca la opción deseada:" << endl;
+    cin >> seletectOption;
+    while (!(0 <= seletectOption && seletectOption <= 5)) {
+        cout << "Introdujo una opción incorrecta, por favor seleccione una opción válida:";
+        cin >> seletectOption;
+    }
+
+    return seletectOption;
+}
