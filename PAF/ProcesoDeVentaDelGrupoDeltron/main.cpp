40225d31d4ac7c515e82cfef707b853a2b526e04
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-12-19 19:43:50 -0500

    Update main.cpp

--- PAF/ProcesoDeVentaDelGrupoDeltron/main.cpp
+++ PAF/ProcesoDeVentaDelGrupoDeltron/main.cpp
@@ -11,6 +11,10 @@ int main()
     // Inicializacion
     inicializacion(deltronXpand);
 	
+    // Muestra el titulo de la app
+    mostrarAppTitulo();
+    mostrarTituloDeOpcion("Por favor introduzca la fecha:");
+
 	// Inicializa la fecha
 	deltronXpand.fecha = pedirFecha();
 	
